0a90a8367fbd16a3882191392e6c9c0f13a79bfc
Igor Ivanov <ivanov.igor.it@gmail.com>
2019-01-01 12:31:56 +0300

    Initial Commit

---
+++ MvvMcoord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MvvMcoord
+//
+//  Created by Igor Ivanov on 01/01/2019.
+//  Copyright © 2019 Igor Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
